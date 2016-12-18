3559d3ef80e3c7cf7c3d8a1cd0e51e43c36e5f3a
Dimitri Korsch <korschdima@gmail.com>
2016-12-18 23:43:06 +0100

    added some channel outputs

--- chainer_mb.py
+++ chainer_mb.py
@@ -8,15 +8,19 @@ import chainer, \
 clip = lambda val: F.clip(val, 0, 6)
 
 class Mixed1(chainer.Chain):
-	def __init__(self, pooling = "avg"):
+	def __init__(self, outputs, pooling = "avg"):
+		assert len(outputs) == 7, "there should be 7 output counts"
 		super(Mixed1, self).__init__(
-			conv1x1 = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),	
-			conv3x3_bottleneck = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),	
-			conv3x3 = L.Convolution2D(None, ..., ksize=3, stride=1, pad=2),	
-			conv3x3d_bottleneck = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),	
-			conv3x3d_pre = L.Convolution2D(None, ..., ksize=3, stride=1, pad=2),	
-			conv3x3d = L.Convolution2D(None, ..., ksize=3, stride=1, pad=2),	
-			pool_reduce = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),	
+			conv1x1 = L.Convolution2D(None, outputs[0], ksize=1, stride=1, pad=2),
+
+			conv3x3_bottleneck = L.Convolution2D(None, outputs[1], ksize=1, stride=1, pad=2),
+			conv3x3 = L.Convolution2D(None, outputs[2], ksize=3, stride=1, pad=2),
+
+			conv3x3d_bottleneck = L.Convolution2D(None, outputs[3], ksize=1, stride=1, pad=2),
+			conv3x3d_pre = L.Convolution2D(None, outputs[4], ksize=3, stride=1, pad=2),
+			conv3x3d = L.Convolution2D(None, outputs[5], ksize=3, stride=1, pad=2),
+
+			pool_reduce = L.Convolution2D(None, outputs[6], ksize=1, stride=1, pad=2),
 		)
 		self.pooling = F.average_pooling_2d if pooling == "avg" else F.max_pooling_2d
 
@@ -38,13 +42,13 @@ class Mixed1(chainer.Chain):
 		return [h1, h2, h3, h4]
 
 class Mixed2(chainer.Chain):
-	def __init__(self):
+	def __init__(self, outputs):
 		super(Mixed2, self).__init__(
-			conv3x3_bottleneck = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),	
-			conv3x3 = L.Convolution2D(None, ..., ksize=3, stride=2, pad=2),	
-			conv3x3d_bottleneck = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),	
-			conv3x3d_pre = L.Convolution2D(None, ..., ksize=3, stride=1, pad=2),	
-			conv3x3d = L.Convolution2D(None, ..., ksize=3, stride=2, pad=2),	
+			conv3x3_bottleneck = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),
+			conv3x3 = L.Convolution2D(None, ..., ksize=3, stride=2, pad=2),
+			conv3x3d_bottleneck = L.Convolution2D(None, ..., ksize=1, stride=1, pad=2),
+			conv3x3d_pre = L.Convolution2D(None, ..., ksize=3, stride=1, pad=2),
+			conv3x3d = L.Convolution2D(None, ..., ksize=3, stride=2, pad=2),
 		)
 
 	def __call__(self, X):
@@ -64,23 +68,24 @@ class Mixed2(chainer.Chain):
 
 class Net(chainer.Chain):
 	def __init__(self):
+		outputs =
 		super(Net, self).__init__(
-			conv0 = L.Convolution2D(None, ..., kernel=7, stride=2, pad=2),
-			conv1 = L.Convolution2D(None, ..., kernel=1, stride=1, pad=1),
-			conv2 = L.Convolution2D(None, ..., kernel=3, stride=1, pad=2),
-			
-			mixed3a = Mixed1("avg"), 
-			mixed3b = Mixed1("avg"), 
-			mixed3c = Mixed2(), 
+			conv0 = L.Convolution2D(None, 64, kernel=7, stride=2, pad=2),
+			conv1 = L.Convolution2D(None, 64, kernel=1, stride=1, pad=1),
+			conv2 = L.Convolution2D(None, 192, kernel=3, stride=1, pad=2),
 
-			mixed4a = Mixed1("avg"), 
-			mixed4b = Mixed1("avg"), 
-			mixed4c = Mixed1("avg"), 
-			mixed4d = Mixed1("avg"), 
-			mixed43 = Mixed2(), 
-			
-			mixed5a = Mixed1("avg"), 
-			mixed5b = Mixed1("max"), 
+			mixed3a = Mixed1(outputs=[64, 64, 64, 64, 96, 96, 32], pooling="avg"),
+			mixed3b = Mixed1(outputs=[64, 64, 96, 64, 96, 96, 64], pooling="avg"),
+			mixed3c = Mixed2(outputs=[]),
+
+			mixed4a = Mixed1(outputs=[224, 64,  96,  96,  128, 128, 128], pooling="avg"),
+			mixed4b = Mixed1(outputs=[192, 96,  128, 96,  128, 128, 128], pooling="avg"),
+			mixed4c = Mixed1(outputs=[160, 128, 160, 128, 160, 160, 96], pooling="avg"),
+			mixed4d = Mixed1(outputs=[96,  128, 192, 160, 192, 192, 96], pooling="avg"),
+			mixed4e = Mixed2(outputs=[]),
+
+			mixed5a = Mixed1(outputs=[352, 192, 320, 160, 224, 224, 128], pooling="avg"),
+			mixed5b = Mixed1(outputs=[352, 192, 320, 192, 224, 224, 128], pooling="max"),
 
 			fc6 = L.Linear(None, ...),
 			fc7 = L.Linear(None, ...),
@@ -115,3 +120,20 @@ class Net(chainer.Chain):
 
 		return self.confidence(h), self.confidence(location)
 
+
+
+import glob, pickle, simplejson as json
+
+if __name__ == '__main__':
+	res = {}
+	for fname in glob.glob("tensors/*.pkl"):
+		with open(fname, "rb") as f:
+			tensor = pickle.load(f, encoding="latin1")
+			if tensor.ndim == 4:
+				n_outs, h, w, channels = tensor.shape
+				res[fname] = dict(outputs=n_outs, h=h, w=w, channels=channels)
+			else:
+				res[fname] = dict(channels=tensor.shape[0])
+
+	with open("layer_info.json", "w") as out:
+		json.dump(res, out, indent = 2)
